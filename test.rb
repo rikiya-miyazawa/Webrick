1b724f4a60dc19dda55a445625d29604f9845030
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-11-13 15:39:16 +0900

    Internal Server Errorが出てしまう

--- test.rb
+++ test.rb
@@ -14,4 +14,13 @@ server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 # この一行を追記
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.mount('/', WEBrick::HTTPServlet::CGIHandler, 'kadai.html.erb')
+
+server.mount('/give_for.cgi', WEBrick::HTTPServlet::CGIHandler, 'give_for.rb')
+
+server.mount('/quality.cgi', WEBrick::HTTPServlet::CGIHandler, 'quality.rb')
+
+
 server.start
